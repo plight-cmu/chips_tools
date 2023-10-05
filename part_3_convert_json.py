b348c34173113ed67715164e423c18f380f08fac
mkshin75 <minkyungshin75@gmail.com>
2023-10-05 13:25:28 -0400

    Update part_3_convert_json.py

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -14,33 +14,35 @@ import json
 
 
 def make_CClvPack_from_json( json_data ):
-    new_level = CCLevel()
-    new_level_pack = CCLevelPack()
-    new_field3 = CCMapTitleField(" ")
-    new_field6 = CCEncodedPasswordField("0000")
-    new_field7 = CCMapHintField(" ")
-    new_field8 = CCMonsterMovementField(" ")
+   
+    game_level_pack = CCLevelPack()
     
+        
     for lv in cclv_json_data:
+        game_level = CCLevel()
+        game_field3 = CCMapTitleField(" ")
+        game_field6 = CCEncodedPasswordField("0000")
+        game_field7 = CCMapHintField(" ")
+        game_field8 = CCMonsterMovementField(" ")
         
-        new_level.level_number = json_data[lv]["Level"]
-        new_level.time = json_data[lv]["Time limit"]
-        new_level.num_chips = json_data[lv]["Chip number"]
-        new_level.upper_layer = json_data[lv]["Upper layer"]
-        new_level.lower_layer = json_data[lv]["Lower layer"]
+        game_level.level_number = json_data[lv]["Level"]
+        game_level.time = json_data[lv]["Time limit"]
+        game_level.num_chips = json_data[lv]["Chip number"]
+        game_level.upper_layer = json_data[lv]["Upper layer"]
+        game_level.lower_layer = json_data[lv]["Lower layer"]
         
-        new_field3.title = json_data[lv]["Map Title Field (type=3)"]
-        new_field6.password = json_data[lv]["Encoded Password Field (type=6)"]
-        new_field7.hint = json_data[lv]["Map Hint Field (type=7)"]
-        new_field8.monsters = json_data[lv]["Moving Objects Field (type=10)"]
+        game_field3.title = json_data[lv]["Map Title Field (type=3)"]
+        game_field6.password = json_data[lv]["Encoded Password Field (type=6)"]
+        game_field7.hint = json_data[lv]["Map Hint Field (type=7)"]
+        game_field8.monsters = json_data[lv]["Moving Objects Field (type=10)"]
         
-        new_level.optional_fields.append(new_field3)
-        new_level.optional_fields.append(new_field6)
-        new_level.optional_fields.append(new_field7)
+        game_level.optional_fields.append(game_field3)
+        game_level.optional_fields.append(game_field6)
+        game_level.optional_fields.append(game_field7)
 
-        new_level_pack.add_level(new_level)
+        game_level_pack.add_level(game_level)
 
-    return new_level_pack
+    return game_level_pack
     
 
 input_json_file = "data/minkyuns_cc1.json"
